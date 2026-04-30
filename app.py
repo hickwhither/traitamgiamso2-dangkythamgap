4633d8a54f6a5afcb2193528c73bc7b41f1f115d
hickwhither <khiemkrkt@gmail.com>
2026-04-30 16:06:48 +0000

    sửa tí hjhj

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-# import eventlet
-# from eventlet import wsgi
-
 from website import *
 from dotenv import load_dotenv
 load_dotenv()
@@ -11,6 +8,5 @@ app = create_app()
 
 
 if __name__ == '__main__':
-    app.run("0.0.0.0", 3273, True) # Debug
-    # wsgi.server(eventlet.listen(("0.0.0.0", 80)), app) # Stable run
+    app.run("0.0.0.0", 3273, True)
 
